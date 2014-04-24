d241a6219b8d456267734bf79815d7e5fb0519c5
Topi Matikainen <topi.matikainen@gmail.com>
2014-04-24 11:04:28 +0300

    Fix mapsApps.js

--- js/mapsApp.js
+++ js/mapsApp.js
@@ -11,7 +11,7 @@ function init() {
         scrollwheel: false,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(66.505648,25.688503), // Rovaniemi
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
